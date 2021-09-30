9f2a13451704376e8e5ca7ea6381278930f802ae
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-30 17:10:03 +0530

    Hemanth : Inventory Management using JSON file

---
+++ 2InventoryMgmt/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _2InventoryMgmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Quantity:");
+            int quantity = int.Parse(Console.ReadLine());
+            JsonFile jfile = new JsonFile();
+            Rice ricedata = jfile.Read("D:/Rice.json");
+
+            for (int i = 0; i < ricedata.TypesOfPulses.Count; i++)
+            {
+                Console.WriteLine(ricedata.TypesOfPulses[i].name);
+                Console.WriteLine(ricedata.TypesOfPulses[i].Price);
+                Console.WriteLine(ricedata.TypesOfPulses[i].weight);
+                int price = ricedata.TypesOfPulses[i].Price;
+                int total = price * quantity;
+                Console.WriteLine("total Price:"+total);
+
+                Console.WriteLine("------");
+            }
+
+            for (int i = 0; i < ricedata.TypesOfRice.Count; i++)
+            {
+                Console.WriteLine(ricedata.TypesOfRice[i].name);
+                Console.WriteLine(ricedata.TypesOfRice[i].Price);
+                Console.WriteLine(ricedata.TypesOfRice[i].weight);
+                int price = ricedata.TypesOfRice[i].Price;
+                int total = price * quantity;
+                Console.WriteLine("total Price:" + total);
+                Console.WriteLine("------");
+            }
+
+        }
+    }
+}
---
+++ 2InventoryMgmt/Rice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2InventoryMgmt
+{
+   
+        public class Rice
+        {
+            public List<TypesOfRice> TypesOfRice;
+            public List<TypesOfPulses> TypesOfPulses;
+        }
+
+        public class TypesOfRice
+        {
+            public string name;
+            public string weight;
+            public int Price;
+        }
+
+        public class TypesOfPulses
+        {
+            public string name;
+            public string weight;
+            public int Price;
+        }
+    
+}
