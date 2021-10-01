b66e6a6873297c612a51d2748b1e42291859bb75
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-01 18:26:26 +0530

    Hemanth : Stock Management System Using Json

---
+++ StockManage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StockManage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stockReport = 0; int total = 0;
+            StockFile stock = new StockFile();
+            Stock data = stock.Read("D:/Stock.json");
+            for (int i = 0; i < data.TypesOfStocks.Count; i++)
+            {
+
+                Console.WriteLine(data.TypesOfStocks[i].name);
+                Console.WriteLine(data.TypesOfStocks[i].price);
+                Console.WriteLine(data.TypesOfStocks[i].shares);
+                total = data.TypesOfStocks[i].price * data.TypesOfStocks[i].shares;
+                Console.WriteLine("total value of " + data.TypesOfStocks[i].name + " is " + total);
+                stockReport += total;
+                Console.WriteLine("------");
+            }
+            Console.WriteLine("Total stock report : " + stockReport);
+        }
+    }
+}
---
+++ StockManage/Stock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockManage
+{
+    class Stock
+    {
+        public List<TypesOfStocks> TypesOfStocks;
+    }
+
+    public class TypesOfStocks
+    {
+        public string name;
+        public int shares;
+        public int price;
+    }
+}
