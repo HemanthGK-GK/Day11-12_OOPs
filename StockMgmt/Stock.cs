2fdc412be1681f141bb6ecc766f250b88de3132d
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-01 18:04:12 +0530

    Hemanth : Stock management using Json files

---
+++ StockMgmt/Stock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockMgmt
+{
+    class Stock
+    {
+        public List<TypesOfStocks> TypesOfStocks;
+    }
+
+    public class TypesOfStocks
+    {
+        public string name;
+        public int shares;
+        public int price;
+    }
+}
---
+++ StockMgmt/StockFile.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace StockMgmt
+{
+    class StockFile
+    {
+        public Stock Read(string Path)
+        {
+            using(StreamReader file = new StreamReader(Path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Stock>(json);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return null;
+                }
+
+            }
+        }
+
+    }
+}
